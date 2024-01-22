de56787cb9dc4157de6332bc888f0a345939f994
Gianlucca <agianlucca@gmail.com>
2024-01-22 15:36:54 +0100

    Link start

---
+++ Conferity/ConferityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConferityApp.swift
+//  Conferity
+//
+//  Created by Gianlucca Annichiarico on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConferityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
